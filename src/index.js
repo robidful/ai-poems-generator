af9e16a3ffcc330bb1d194a572621abe4fcf7f03
robidful <150020689+robidful@users.noreply.github.com>
2023-11-27 14:13:48 +0000

    Improved user experience

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 function displayPoem(response) {
+  let waitingElement = document.querySelector(".waiting-message");
+  waitingElement.style.display = "none";
+
   new Typewriter("#actual-poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -17,6 +20,9 @@ function generatePoem(event) {
   let context =
     "You are a romantic yet ironic expert writer and you love generating poems.Your mission is to generate 6 lines poem, making sure to follow the user instructions. Make sure to display a new line of text whenever there is a new verse, like a proper poem would be written.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let waitingElement = document.querySelector(".waiting-message");
+  waitingElement.innerHTML = `Generating a sublime poem about ${userInstructions.value}...`;
 
   axios.get(apiUrl).then(displayPoem);
 }
