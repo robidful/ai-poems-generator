e726e741e882e03327ef995b19bc27caf0218aae
robidful <150020689+robidful@users.noreply.github.com>
2023-11-27 13:29:21 +0000

    implemented API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#actual-poem", {
-    strings: "Hello, I am a poet",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let userInstructions = document.querySelector("#prompt");
+
+  let apiKey = "47a73a37647027b080t9do6f11739604";
+  let prompt = `User instructions: Generate a poem in English about ${userInstructions.value}`;
+  let context =
+    "You are a romantic yet ironic expert writer and you love generating poems.Your mission is to generate 6 lines poem, making sure to follow the user instructions. Make sure to start a new paragraph of text whenever there is a new verse, like a real poem would be written.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
